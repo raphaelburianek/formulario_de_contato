3727a8e7d8b1869f3e9718fa513cb1dd4af8cfbe
Raphael Burianek <raphael.burianek@gmail.com>
2021-05-25 22:20:35 -0300

    resolvendo error 500

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset$_POST['email']) && !empty($_POST['email']){
+if(isset($_POST['email']) && !empty($_POST['email'])){
 
 $nome = addslashes($_POST['name']);
 $email = addslashes($_POST['email']);
@@ -9,17 +9,17 @@ $mensagem = addslashes($_POST['message']);
 
 $to ="raphael.burianek@mail.com";
 $subjet = "Contato - formulario";
-$body = "Nome: ".$nome. "\r\n"
-        "Email: ".$email. "\r\n"
+$body = "Nome: ".$nome. "\r\n".
+        "Email: ".$email. "\r\n".
         "Mensagem: ".$mensagem;
-$header = "from: burianekraphael@gmail.com". "\r\n" 
-            ."Reply-to: ".$email."\r\n"
-            ."X=Mailer:PHP/".phpversion();
+$header = "from: burianekraphael@gmail.com". "\r\n" .
+            "Reply-to: ".$email."\r\n".
+            "X=Mailer:PHP/".phpversion();
 if(mail($to,$subjet,$body,$header)){
 
     echo("Email enviado com sucesso!");
 }else{
-    echo("O Email não pode ser enviado")";"
+    echo("O Email não pode ser enviado");
 }
 
 
